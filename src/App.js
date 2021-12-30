cb3c4ac9dd9b5a4916fde54bd7eeb5cd77ca6f1b
mustafa <mltm0204@gmail.com>
2021-12-30 14:45:44 +0300

    router version5 added

--- src/App.js
+++ src/App.js
@@ -9,9 +9,23 @@ import Error from './pages/Error'
 import Navbar from './components/Navbar'
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+    <Router>
+        <Navbar/>
+        <Switch>
+            <Route exact path="/" >
+                <Home/>
+            </Route>
+            <Route path="/about" >
+                <Home/>
+            </Route>
+            <Route path="/coctail/:id" >
+                <SingleCocktail/>
+            </Route>
+            <Route path="*" >
+                <Error/>
+            </Route>
+        </Switch>
+    </Router>
   )
 }
 
